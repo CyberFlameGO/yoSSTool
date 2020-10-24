02ca486a79132b8844f60d2ee8800f131a1410bd
Yochran <chickensaresavage@gmail.com>
2020-10-24 19:45:59 -0400

    Initial Commit

---
+++ yoSSTool/ScreenshareTool.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace yoSSTool
+{
+    partial class SSTool
+    {
+        private System.ComponentModel.IContainer components = null;
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SSTool));
+            this.ph2 = new System.Windows.Forms.Button();
+            this.luyten = new System.Windows.Forms.Button();
+            this.everything = new System.Windows.Forms.Button();
+            this.uav = new System.Windows.Forms.Button();
+            this.lav = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ph2
+            // 
+            this.ph2.Location = new System.Drawing.Point(12, 12);
+            this.ph2.Name = "ph2";
+            this.ph2.Size = new System.Drawing.Size(198, 44);
+            this.ph2.TabIndex = 0;
+            this.ph2.Text = "Process Hacker 2";
+            this.ph2.UseVisualStyleBackColor = true;
+            this.ph2.Click += new System.EventHandler(this.ph2_Click);
+            // 
+            // luyten
+            // 
+            this.luyten.Location = new System.Drawing.Point(216, 12);
+            this.luyten.Name = "luyten";
+            this.luyten.Size = new System.Drawing.Size(177, 44);
+            this.luyten.TabIndex = 1;
+            this.luyten.Text = "Luyten";
+            this.luyten.UseVisualStyleBackColor = true;
+            this.luyten.Click += new System.EventHandler(this.luyten_Click);
+            // 
+            // everything
+            // 
+            this.everything.Location = new System.Drawing.Point(399, 12);
+            this.everything.Name = "everything";
+            this.everything.Size = new System.Drawing.Size(189, 44);
+            this.everything.TabIndex = 2;
+            this.everything.Text = "Search Everything";
+            this.everything.UseVisualStyleBackColor = true;
+            this.everything.Click += new System.EventHandler(this.everything_Click);
+            // 
+            // uav
+            // 
+            this.uav.Location = new System.Drawing.Point(12, 60);
+            this.uav.Name = "uav";
+            this.uav.Size = new System.Drawing.Size(198, 46);
+            this.uav.TabIndex = 3;
+            this.uav.Text = "User Assist View";
+            this.uav.UseVisualStyleBackColor = true;
+            this.uav.Click += new System.EventHandler(this.uav_Click);
+            // 
+            // lav
+            // 
+            this.lav.Location = new System.Drawing.Point(217, 60);
+            this.lav.Name = "lav";
+            this.lav.Size = new System.Drawing.Size(176, 46);
+            this.lav.TabIndex = 4;
+            this.lav.Text = "Last Activity Viewer";
+            this.lav.UseVisualStyleBackColor = true;
+            this.lav.Click += new System.EventHandler(this.lav_Click_1);
+            // 
+            // SSTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 250);
+            this.Controls.Add(this.lav);
+            this.Controls.Add(this.uav);
+            this.Controls.Add(this.everything);
+            this.Controls.Add(this.luyten);
+            this.Controls.Add(this.ph2);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Name = "SSTool";
+            this.Text = "Screenshare Tool";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button lav;
+
+        private System.Windows.Forms.Button uav;
+
+        private System.Windows.Forms.Button everything;
+
+        private System.Windows.Forms.Button luyten;
+        
+        private System.Windows.Forms.Button ph2;
+
+        #endregion
+    }
+}
---
+++ yoSSTool/ScreenshareTool.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yoSSTool
+{
+    public partial class SSTool : Form
+    {
+        public SSTool()
+        {
+            InitializeComponent();
+        }
+
+        private void ph2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo();
+            sInfo.WorkingDirectory = "C:/Users/chick/Desktop/Yochran's SS tools";
+            sInfo.FileName = "ProcessHacker.exe";
+            Process.Start(sInfo);
+        }
+
+        private void luyten_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo();
+            sInfo.WorkingDirectory = "C:/Users/chick/Desktop/Yochran's SS tools";
+            sInfo.FileName = "luyten-0.5.4.exe";
+            Process.Start(sInfo);
+        }
+
+        private void everything_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo();
+            sInfo.WorkingDirectory = "C:/Program Files/Everything";
+            sInfo.FileName = "Everything.exe";
+            Process.Start(sInfo);
+        }
+
+        private void uav_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo();
+            sInfo.WorkingDirectory = "C:/Users/chick/Desktop/Yochran's SS tools";
+            sInfo.FileName = "UserAssistView.exe";
+            Process.Start(sInfo);
+        }
+
+        private void lav_Click_1(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo();
+            sInfo.WorkingDirectory = "C:/Users/chick/Desktop/Yochran's SS tools";
+            sInfo.FileName = "LastActivityView.exe";
+            Process.Start(sInfo);
+        }
+    }
+}
