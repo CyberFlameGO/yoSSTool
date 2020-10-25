f911567bc75afe04f6aa1dd1990cb434a7648bb3
Yochran <chickensaresavage@gmail.com>
2020-10-25 15:43:33 -0400

    Finished the SS tool, added JDGui, Process Explorer, and a screenshare logging system.

--- yoSSTool/ScreenshareTool.Designer.cs
+++ yoSSTool/ScreenshareTool.Designer.cs
@@ -15,7 +15,11 @@
         }
 
         #region Windows Form Designer generated code
-        
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SSTool));
@@ -24,6 +28,11 @@
             this.everything = new System.Windows.Forms.Button();
             this.uav = new System.Windows.Forms.Button();
             this.lav = new System.Windows.Forms.Button();
+            this.jdgui = new System.Windows.Forms.Button();
+            this.procexpl = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.logdate = new System.Windows.Forms.Button();
+            this.openPrev = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ph2
@@ -74,13 +83,65 @@
             this.lav.TabIndex = 4;
             this.lav.Text = "Last Activity Viewer";
             this.lav.UseVisualStyleBackColor = true;
-            this.lav.Click += new System.EventHandler(this.lav_Click_1);
+            this.lav.Click += new System.EventHandler(this.lav_Click);
+            // 
+            // jdgui
+            // 
+            this.jdgui.Location = new System.Drawing.Point(399, 60);
+            this.jdgui.Name = "jdgui";
+            this.jdgui.Size = new System.Drawing.Size(188, 45);
+            this.jdgui.TabIndex = 5;
+            this.jdgui.Text = "JD-Gui";
+            this.jdgui.UseVisualStyleBackColor = true;
+            this.jdgui.Click += new System.EventHandler(this.jdguiClick);
+            // 
+            // procexpl
+            // 
+            this.procexpl.Location = new System.Drawing.Point(217, 112);
+            this.procexpl.Name = "procexpl";
+            this.procexpl.Size = new System.Drawing.Size(176, 48);
+            this.procexpl.TabIndex = 6;
+            this.procexpl.Text = "Process Explorer";
+            this.procexpl.UseVisualStyleBackColor = true;
+            this.procexpl.Click += new System.EventHandler(this.procExpClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(217, 204);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(175, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // logdate
+            // 
+            this.logdate.Location = new System.Drawing.Point(17, 195);
+            this.logdate.Name = "logdate";
+            this.logdate.Size = new System.Drawing.Size(192, 42);
+            this.logdate.TabIndex = 8;
+            this.logdate.Text = "Log SS";
+            this.logdate.UseVisualStyleBackColor = true;
+            this.logdate.Click += new System.EventHandler(this.logDate);
+            // 
+            // Open Prev
+            // 
+            this.openPrev.Location = new System.Drawing.Point(399, 195);
+            this.openPrev.Name = "openprev";
+            this.openPrev.Size = new System.Drawing.Size(187, 41);
+            this.openPrev.TabIndex = 9;
+            this.openPrev.Text = "Open Previous SS Log";
+            this.openPrev.UseVisualStyleBackColor = true;
+            this.openPrev.Click += new System.EventHandler(this.openPrevLogClick);
             // 
             // SSTool
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 250);
+            this.Controls.Add(this.openPrev);
+            this.Controls.Add(this.logdate);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.procexpl);
+            this.Controls.Add(this.jdgui);
             this.Controls.Add(this.lav);
             this.Controls.Add(this.uav);
             this.Controls.Add(this.everything);
@@ -91,6 +152,16 @@
             this.Text = "Screenshare Tool";
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button openPrev;
+
+        private System.Windows.Forms.Button logdate;
+        
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+
+        private System.Windows.Forms.Button procexpl;
+
+        private System.Windows.Forms.Button jdgui;
 
         private System.Windows.Forms.Button lav;
 
--- yoSSTool/ScreenshareTool.cs
+++ yoSSTool/ScreenshareTool.cs
@@ -18,11 +18,12 @@ namespace yoSSTool
         {
             InitializeComponent();
         }
+        
 
         private void ph2_Click(object sender, EventArgs e)
         {
             ProcessStartInfo sInfo = new ProcessStartInfo();
-            sInfo.WorkingDirectory = "C:/Users/chick/Desktop/Yochran's SS tools";
+            sInfo.WorkingDirectory = Directory.GetCurrentDirectory();
             sInfo.FileName = "ProcessHacker.exe";
             Process.Start(sInfo);
         }
@@ -30,7 +31,7 @@ namespace yoSSTool
         private void luyten_Click(object sender, EventArgs e)
         {
             ProcessStartInfo sInfo = new ProcessStartInfo();
-            sInfo.WorkingDirectory = "C:/Users/chick/Desktop/Yochran's SS tools";
+            sInfo.WorkingDirectory = Directory.GetCurrentDirectory();
             sInfo.FileName = "luyten-0.5.4.exe";
             Process.Start(sInfo);
         }
@@ -38,7 +39,7 @@ namespace yoSSTool
         private void everything_Click(object sender, EventArgs e)
         {
             ProcessStartInfo sInfo = new ProcessStartInfo();
-            sInfo.WorkingDirectory = "C:/Program Files/Everything";
+            sInfo.WorkingDirectory = Directory.GetCurrentDirectory();
             sInfo.FileName = "Everything.exe";
             Process.Start(sInfo);
         }
@@ -46,17 +47,84 @@ namespace yoSSTool
         private void uav_Click(object sender, EventArgs e)
         {
             ProcessStartInfo sInfo = new ProcessStartInfo();
-            sInfo.WorkingDirectory = "C:/Users/chick/Desktop/Yochran's SS tools";
+            sInfo.WorkingDirectory = Directory.GetCurrentDirectory();
             sInfo.FileName = "UserAssistView.exe";
             Process.Start(sInfo);
         }
 
-        private void lav_Click_1(object sender, EventArgs e)
+        private void lav_Click(object sender, EventArgs e)
         {
             ProcessStartInfo sInfo = new ProcessStartInfo();
-            sInfo.WorkingDirectory = "C:/Users/chick/Desktop/Yochran's SS tools";
+            sInfo.WorkingDirectory = Directory.GetCurrentDirectory();
             sInfo.FileName = "LastActivityView.exe";
             Process.Start(sInfo);
+        }
+
+        private void jdguiClick(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo();
+            sInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+            sInfo.FileName = "jdgui.jar";
+            Process.Start(sInfo);
+        }
+
+        private void procExpClick(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo();
+            sInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+            sInfo.FileName = "procexp.exe";
+            Process.Start(sInfo);
+        }
+
+        private void logDate(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "hh:mm:ss tt dd/MM/yyyy";
+            dateTimePicker1.ShowUpDown = true;
+            string dt = dateTimePicker1.Value.ToString("hh:mm:ss tt dd/MM/yyyy");
+            string path = @"C:\Temp\ScreenshareLog.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("This user has been screenshared before.");
+                    sw.WriteLine("SS Date: " + dt);
+                    sw.WriteLine(" ");
+                }   
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine("This user has been screenshared before.");
+                    sw.WriteLine("SS Date: " + dt);
+                    sw.WriteLine(" ");
+                }
+            }
+        }
+
+        private void openPrevLogClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"C:\Temp\ScreenshareLog.txt";
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    ProcessStartInfo sInfo = new ProcessStartInfo();
+                    sInfo.WorkingDirectory = "C:/Temp";
+                    sInfo.FileName = "ScreenshareLog.txt";
+                    Process.Start(sInfo);
+                }
+            }
+            catch (FileNotFoundException exception)
+            {
+                string message = "No Screenshare were found for this user.";
+                string top = "Screenshare Logs";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBoxIcon icon = MessageBoxIcon.Error;
+                MessageBox.Show(message, top, buttons, icon);
+            }
         }
     }
 }
